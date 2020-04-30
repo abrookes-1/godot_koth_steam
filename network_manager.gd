aaa16528c77d14d88d2625a8210a7272840484ab
Aidan Brookes <aaabrookes@gmail.com>
2020-04-29 21:30:19 -0600

    fixed sending params in packet

--- network_manager.gd
+++ network_manager.gd
@@ -39,13 +39,14 @@ func _process_server_updates():
 func add_networked_node(node):
 	net_nodes[node.net_id] = node
 
-func send_spawn(type, position, net_id):
+func send_spawn(type, position, net_id, params):
 	var DATA = PoolByteArray()
 	var d = {
 		'directive': 'spawn',
 		'type': type,
 		'net_id': net_id,
 		'position': position,
+		'params': params
 	}
 	DATA.append(256)
 	DATA.append_array(var2bytes(d))
@@ -64,13 +65,17 @@ func send_position(node):
 	steam_controller._send_P2P_Packet(DATA, 1, 0)
 	
 func do_position_directive(data):
-	net_nodes[data['net_id']].set_pos(data['position'])
+	if net_nodes.has(data['net_id']):
+		net_nodes[data['net_id']].set_pos(data['position'])
+	else:
+		print('Warning: tried to move nonexistent net_id ' + str(data['net_id']))
 
 func do_spawn_directive(data):
 	# spawn new object
 	var new_player = spawnable[data['type']].instance()
 	new_player.set_pos(data['position'])
 	new_player.net_id = data['net_id']
+	new_player.spawn_params = data['params']
 	get_tree().get_root().add_child(new_player)
 	# push to networked objects
 	add_networked_node(new_player)
@@ -86,7 +91,7 @@ func spawn_new_networked(type, position, params={}):
 	# push to networked objects
 	add_networked_node(new_player)
 	# send spawn directive to clients
-	send_spawn(type, position, network_id)
+	send_spawn(type, position, network_id, params)
 	
 func get_new_id():
 	id_counter = id_counter + 1
