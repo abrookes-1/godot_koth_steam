66bc014a97e565b4d0b8c043f9e2275ceb312583
Aidan Brookes <aaabrookes@gmail.com>
2020-04-30 01:49:14 -0600

    ui stuff

--- assets/ui/scripts/ui_opener.gd
+++ assets/ui/scripts/ui_opener.gd
@@ -2,4 +2,9 @@ extends Node
 
 func _input(event):
 	if Input.is_action_just_pressed("in_game_menu"):
-		get_parent().visible = !get_parent().visible
+		if get_parent().visible == true:
+			get_parent().set_visible(false)
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			get_parent().set_visible(true)
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
