3fdcd3f65b641661a28269bb9389426035ab8946
Aidan Brookes <aaabrookes@gmail.com>
2020-04-30 20:29:41 -0600

    player movement  tweaks

--- characters/player/fp_controller.gd
+++ characters/player/fp_controller.gd
@@ -4,7 +4,7 @@ var camera_angle = 0
 var mouse_sensitivity = 0.1
 
 var velocity = Vector3()
-var direction = Vector3()
+var input_direction = Vector3()
 
 const FLY_SPEED = 40
 const FLY_ACCEL = 4
@@ -15,7 +15,8 @@ const MAX_RUNNING_SPEED = 20
 const ACCEL = 2
 const DECEL = 20
 const AIR_ACCEL = 1
-var jump_height = 15
+const AIR_DECEL = DECEL
+var jump_height = 12
 var temp = 0
 
 var spawn_params # set on spawn, do stuff with it in ready
@@ -62,7 +63,7 @@ func _input(event):
 		$Head.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
 		
 		var change = -event.relative.y * mouse_sensitivity
-		if change + camera_angle < 90 and change + camera_angle > -90:
+		if change + camera_angle < 89 and change + camera_angle > -89:
 			$Head/Camera.rotate_x(deg2rad(change))
 			camera_angle += change
 			
@@ -71,21 +72,23 @@ func _input(event):
 			weapon.fire_weapon()
 
 
-
 func _walk(delta):
-	direction = Vector3()
+	input_direction = Vector3()
 	var facing = $Head/Camera.get_camera_transform().basis
 	
 	if Input.is_action_pressed("move_fw"):
-		direction -= facing.z
+		input_direction -= facing.z
 	if Input.is_action_pressed("move_bw"):
-		direction += facing.z
+		input_direction += facing.z
 	if Input.is_action_pressed("move_l"):
-		direction -= facing.x
+		input_direction -= facing.x
 	if Input.is_action_pressed("move_r"):
-		direction += facing.x
+		input_direction += facing.x
 
-	direction = direction.normalized()
+	input_direction = input_direction.normalized()
+	
+	
+	
 	
 	if (is_on_floor()):
 		has_contact = true
@@ -106,28 +109,55 @@ func _walk(delta):
 	else:
 		speed = MAX_SPEED
 	
-	var target = direction * speed
+	var target = input_direction * speed
 	
 	# determine if accelerating or decelerating
+	var is_accel = false
+	if input_direction.dot(temp_velocity) > 0:
+		is_accel = true
 	var acceleration
-	if !has_contact:
-		acceleration = AIR_ACCEL
-	elif direction.dot(temp_velocity) > 0:
-		acceleration = ACCEL
+	
+#	if is_accel:
+#		if has_contact:
+#			acceleration = ACCEL # accelerating on ground
+#		else:
+#			acceleration = AIR_ACCEL # accelerating in air
+#	else:
+#		if has_contact:
+#			acceleration = DECEL # decelerating on ground
+#		else:
+#			acceleration = AIR_DECEL # decelerating in the air
+	
+	if has_contact:
+		if is_accel:
+			acceleration = ACCEL # accelerating on ground
+		else:
+			acceleration = DECEL # decelerating on ground
 	else:
-		acceleration = DECEL
+		if is_accel or input_direction == Vector3.ZERO:
+			acceleration = AIR_ACCEL # accelerating in air
+		else:
+			acceleration = AIR_DECEL # decelerating in the air
+	
+#	if !has_contact:
+#		acceleration = AIR_ACCEL
+#	elif input_direction.dot(temp_velocity) > 0:
+#		acceleration = ACCEL
+#	else:
+#		acceleration = DECEL
 	
 	temp_velocity = temp_velocity.linear_interpolate(target, acceleration * delta)
 	
 	velocity.x = temp_velocity.x
 	velocity.z = temp_velocity.z
-
+	
+	# jump if possible
 	if has_contact and Input.is_action_just_pressed("move_jump"):
 		velocity.y = jump_height
 		has_contact = false
 
-	velocity = move_and_slide(velocity, Vector3(0,1,0))
 
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
 
 
 func _do_gravity(delta):
@@ -145,20 +175,20 @@ func is_on_floor_or_slope():
 	return true
 
 func _fly(delta):
-	direction = Vector3()
+	input_direction = Vector3()
 	var facing = $Head/Camera.get_camera_transform().basis
 	
 	if Input.is_action_pressed("move_fw"):
-		direction -= facing.z
+		input_direction -= facing.z
 	if Input.is_action_pressed("move_bw"):
-		direction += facing.z
+		input_direction += facing.z
 	if Input.is_action_pressed("move_l"):
-		direction -= facing.x
+		input_direction -= facing.x
 	if Input.is_action_pressed("move_r"):
-		direction += facing.x
+		input_direction += facing.x
 
-	direction = direction.normalized()
-	var target = direction * FLY_SPEED
+	input_direction = input_direction.normalized()
+	var target = input_direction * FLY_SPEED
 	velocity = velocity.linear_interpolate(target, FLY_ACCEL * delta)
 	
 	move_and_slide(velocity)
