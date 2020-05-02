ffd6b941577ca290706fafdcc20a5c432d687cb8
Erikson <eriksonjthomas@gmail.com>
2020-05-02 12:21:40 -0700

    uopdate

--- ui/steam/steam_controller.gd
+++ ui/steam/steam_controller.gd
@@ -273,10 +273,11 @@ func _read_P2P_Packet():
 
 		# Print the packet to output
 		#print("Packet: "+str(READABLE))
+		packets_read += 1
+		print("packets read" + packets_read)
 		return READABLE # Append logic here to deal with packet data
 		
-		packets_read += 1
-		print(packets_read)
+
 
 func _send_P2P_Packet(data, send_type, channel):
 	
@@ -288,7 +289,7 @@ func _send_P2P_Packet(data, send_type, channel):
 			if MEMBER['steam_id'] != STEAM_ID:
 				
 				packets_sent += 1
-				print(packets_sent)
+				print("packets sent" + packets_sent)
 				Steam.sendP2PPacket(MEMBER['steam_id'], data, send_type, channel)
 
 func _on_P2P_Session_Connect_Fail(lobbyID, session_error):
